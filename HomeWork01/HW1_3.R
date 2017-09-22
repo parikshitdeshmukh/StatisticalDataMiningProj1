325f14728c00565fc2f21ab2064e94138c801fd7
Parikshit Deshmukh <parikshitd92@gmail.com>
2017-09-22 12:36:15 -0400

    null

--- HomeWork01/HW1_3.R
+++ HomeWork01/HW1_3.R
@@ -17,35 +17,48 @@ dim(zipTr)
 zipTest <- as.data.frame(zip.test)
 #zipTest[, 1] <- as.factor(zipTest[, 1])  
 zipTest <- zipTest[zipTest$V1 ==2 | zipTest$V1 == 3,]
-dim(zipTest)system.time(
+dim(zipTest)
 
 
 ########################################################################################################
 ##>>>>>>>>>USING KNN CLASSIFICATION<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 ########################################################################################################
-
-system.time(model.knn <- knn(zipTr, zipTest, zipTr$V1, k = 2))
-summary(model.knn)
-str(model.knn)
-
-model.knn <-as.data.frame(model.knn)
-knn<-model.knn$model.knn[1:364]
-# 
-# #####USING MODEL TO PREDICT TEST DATA
-# prediction.knn2 <- predict(model.knn2 ,  zipTest, type = "class")
-#### TABULATING RESULTS
-table(`Actual Class` = zipTest$V1, `Predicted Class` = knn)
-
-##ERROR RATE
-error.rate.knn <- sum(zipTest$V1!= knn)/nrow(zipTest)## >>>>>>>>>>>> 0.02197802
-print(paste0("Accuary (Precision): ", 1 - error.rate.knn)) ## >>>>>>>> 0.978022
-
-
-for (i in 1:10){
-  p <- knnplot(train, test, i)
-  ggsave(filename = filer[i], plot = p, height = 5, width = 5)
+knnCompute<- function(train, test, cl, k){
+            
+          system.time(model.knn <- knn(train, test, cl, k))
+          return(model.knn)
 }
 
+k=1
+error.rate.knn=0
+
+for (i in seq(1,15,2)){
+  model.knn=knnCompute(zipTr, zipTest, zipTr$V1, i);
+  model.knn <-as.data.frame(model.knn);
+  knn<-model.knn$model.knn[1:364];
+  error.rate.knn[k] <- sum(zipTest$V1!= knn)/nrow(zipTest);
+  print(paste0("Accuary (Precision): ", 1 - error.rate.knn));
+  k=k+1;
+}
+print(error.rate.knn)
+
+
+################################
+##>> PLotting Error for k=1,3,5,7,9,11,13,15
+##################################
+
+m=c(1:8)
+plot(m, error.rate.knn,type = "l" , main="Error rate Vs K",xlab = "K", ylab = "Error Rate")
+
+# 
+# #####USING MODEL TO PREDICT TEST DATA
+#prediction.knn2 <- predict(model.knn ,  zipTest, type = "class")
+#### TABULATING RESULTS
+#table(`Actual Class` = zipTest$V1, `Predicted Class` = knn)
+
+##Minimum ERROR RATE
+min.error.rate.knn <- min(error.rate.knn)## >>>>>>>>>>>> 0.02472527
+print(paste0("Accuary (Precision): ", 1 - min.error.rate.knn)) ## >>>>>>>> 0.978022
 
 
 
@@ -57,7 +70,7 @@ summary(lr.model)##>>>>>>>>>>>>>>>>>>>>> We get RSE here
 
 system.time(prediction.lr <- predict(lr.model, zipTest, se.fit = TRUE))
 lr<- as.data.frame(prediction.lr$fit)
-lr<- lr$`prediction.lr$fit
+lr<- lr$`prediction.lr$fit`
 
 #### APPROXIMATING THE VALUE TO NEAREST INTEGER 2 OR 3
 for (i in 1:364){
@@ -66,25 +79,17 @@ for (i in 1:364){
   
 }
 
+### ERROR RATE
 error.rate.lr <- sum(zipTest$V1 != lr)/nrow(zipTest) #### >>>> 0.04120879
 print(paste0("Accuary (Precision): ", 1 - error.rate.lr)) ## >>>>>>>>  0.9587912
 
 table(`Actual Class` = zipTest$V1, `Predicted Class` =lr)
 
 
-
-
-
-
-
-
-
 ###############################################
 ##
 ## Since the erro rate for KNN is o.o2 and for Linear Regression 0.04, it can be concluded
 ## that the KNN Cassification algorithm is more accurate.
-##
-##
 ##
 ##
 ###############################################
